cca1445f15a4b33dfe17745fef3c6f1def632c5d
Laurakatrine <32058371+Laurakatrine@users.noreply.github.com>
2018-10-20 18:20:56 +0200

    motionsdata

---
+++ motionsdata.js
@@ -0,0 +1,7 @@
+var data = [{id:1,"first_data":"11-10-2018","last_data":"3000 skridt"},
+           {id:3,"first_data":"12-10-2018","last_data":"8000 skridt"}, 
+           {id:4,"first_data":"13-10-2018","last_data":"9000 skridt"}, 
+           {id:5,"first_data":"Level up!","last_data":"<a href='url'>gense historie</a>"}, 
+           {id:6,"first_data":"14-10-2018","last_data":"3000 skridt"}, ]
+
+var myJSON = JSON.stringify(data);
