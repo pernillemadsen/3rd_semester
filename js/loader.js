5711333f30d9b14bf501fd6643d8075892926a97
Diis <36528782+Diis@users.noreply.github.com>
2018-10-30 18:18:21 +0100

    Revert "Merge branch 'master' of https://github.com/pernillemadsen/3rd_semester"
    This reverts commit 9099a74d820a02423c4371f4c7b1876f81ccb7d3, reversing
    changes made to 44c24f042d801d967b5a5a133d83d0e32ceeb593.

--- js/loader.js
+++ js/loader.js
@@ -1,30 +1,19 @@
 var loggedIn = Boolean(false);
 
 $(document).ready(function () {
-<<<<<<< HEAD
 
     $(".navbar").load("index.html");
-=======
-    
-    $(".navbar").load("navbar.html");
->>>>>>> 5e4aa729dab46156382d58a7f43027b33c0abe6b
     $(".content_master").load("forside.html");
 
 });
 
-<<<<<<< HEAD
 function loadHjem() {
     $(".navbar").load("index.html");
-=======
-function loadHjem(){
-    $(".navbar").load("navbar.html");
->>>>>>> 5e4aa729dab46156382d58a7f43027b33c0abe6b
     $(".content_master").load("forside.html");
 
 }
 
 function loadMotionsdata() {
-<<<<<<< HEAD
     $(".navbar").load("index.html");
     
     if(loggedIn){
@@ -34,26 +23,17 @@ function loadMotionsdata() {
     else {
         $(".content_master").load("loginSide.html");
     }
-=======
-    $(".navbar").load("navbar.html");
-    $(".content_master").load("motionsdata.html");
->>>>>>> 5e4aa729dab46156382d58a7f43027b33c0abe6b
 }
 
 
 function loadSundhedsfakta() {
-    $(".navbar").load("navbar.html");
+    $(".navbar").load("index.html");
     $(".content_master").load("sundhedsfakta.html");
 }
 
 
 function loadOmprojektet() {
-    $(".navbar").load("navbar.html");
+    $(".navbar").load("index.html");
     $(".content_master").load("omProjektet.html");
 
-}
-
-function loadGame () {
-    $(".navbar").load("navbar.html");
-    $(".content_master").load("unitySpil.html");
 }
