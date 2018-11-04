3f4b015e0ca454ec29c33979f857bed0aa6bfe5b
Pernille Madsen <iris.crossblade@gmail.com>
2018-11-04 10:36:12 +0100

    Login
    Ikke færdigt login

--- js/loader.js
+++ js/loader.js
@@ -1,32 +1,34 @@
-var loggedIn = Boolean(false);
+var loggedIn = Boolean(true);
 
 $(document).ready(function () {
-<<<<<<< HEAD
     $(".navbar").load("navbar.html");
-=======
-
-    $(".navbar").load("index.html");
->>>>>>> parent of 9099a74... Merge branch 'master' of https://github.com/pernillemadsen/3rd_semester
     $(".content_master").load("forside.html");
 
 });
 
-function loadHjem() {
-<<<<<<< HEAD
-    $(".navbar").load("navbar.html");
-=======
-    $(".navbar").load("index.html");
->>>>>>> parent of 9099a74... Merge branch 'master' of https://github.com/pernillemadsen/3rd_semester
-    $(".content_master").load("forside.html");
+/*function loggedIn() {
+    loggedIn = Boolean(true);
+}*/
 
+function loadHjem() {
+    $(".navbar").load("navbar.html");
+    $(".content_master").load("forside.html");
+}
+
+function loadSpil(){
+    $(".navbar").load("navbar.html");
+    
+    if(loggedIn){
+        $(".content_master").load("unitySpil.html");
+    }
+    
+    else {
+        $(".content_master").load("loginSide.html");
+    }
 }
 
 function loadMotionsdata() {
-<<<<<<< HEAD
     $(".navbar").load("navbar.html");
-=======
-    $(".navbar").load("index.html");
->>>>>>> parent of 9099a74... Merge branch 'master' of https://github.com/pernillemadsen/3rd_semester
     
     if(loggedIn){
         $(".content_master").load("motionsdata.html");
@@ -39,13 +41,13 @@ function loadMotionsdata() {
 
 
 function loadSundhedsfakta() {
-    $(".navbar").load("index.html");
+    $(".navbar").load("navbar.html");
     $(".content_master").load("sundhedsfakta.html");
 }
 
 
 function loadOmprojektet() {
-    $(".navbar").load("index.html");
+    $(".navbar").load("navbar.html");
     $(".content_master").load("omProjektet.html");
 
 }
