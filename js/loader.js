6ac9a2c753337a17c06039446f09644e32a1461d
Diis <36528782+Diis@users.noreply.github.com>
2018-11-05 12:33:11 +0100

    Revert "Merge branch 'master' of https://github.com/pernillemadsen/3rd_semester"
    This reverts commit bdad8ca17121499273ecff637b0a9a7296c8b588, reversing
    changes made to 127674ef057639e150b78758e2452c18d73b554c.

--- js/loader.js
+++ js/loader.js
@@ -1,53 +1,59 @@
-var loggedIn = Boolean(true);
-
+var loggedIn = Boolean(false);
 
 $(document).ready(function () {
+<<<<<<< HEAD
+
+    $(".navbar").load("index.html");
+=======
+    
     $(".navbar").load("navbar.html");
+>>>>>>> 5e4aa729dab46156382d58a7f43027b33c0abe6b
     $(".content_master").load("forside.html");
-    $(".footer").load("footer.html");
+
 });
 
+<<<<<<< HEAD
 function loadHjem() {
+    $(".navbar").load("index.html");
+=======
+function loadHjem(){
     $(".navbar").load("navbar.html");
+>>>>>>> 5e4aa729dab46156382d58a7f43027b33c0abe6b
     $(".content_master").load("forside.html");
-    $(".footer").load("footer.html");
-}
 
-function loadSpil() {
-    $(".navbar").load("navbar.html");
-    
-    if (loggedIn) {
-        $(".content_master").load("unitySpil.html");
-    }
-
-     else {
-        $(".content_master").load("loginSide.html");
-    }
-    $(".footer").load("footer.html");
 }
 
 function loadMotionsdata() {
-    $(".navbar").load("navbar.html");
-
-    if (loggedIn) {
+<<<<<<< HEAD
+    $(".navbar").load("index.html");
+    
+    if(loggedIn){
         $(".content_master").load("motionsdata.html");
-    } else {
+    }
+    
+    else {
         $(".content_master").load("loginSide.html");
     }
-    $(".footer").load("footer.html");
+=======
+    $(".navbar").load("navbar.html");
+    $(".content_master").load("motionsdata.html");
+>>>>>>> 5e4aa729dab46156382d58a7f43027b33c0abe6b
 }
 
 
 function loadSundhedsfakta() {
     $(".navbar").load("navbar.html");
     $(".content_master").load("sundhedsfakta.html");
-    $(".footer").load("footer.html");
 }
 
 
 function loadOmprojektet() {
     $(".navbar").load("navbar.html");
     $(".content_master").load("omProjektet.html");
-    $(".footer").load("footer.html");
 
+}
+
+function loadGame () {
+    $(".navbar").load("navbar.html");
+    $(".content_master").load("unitySpil.html");
 }
